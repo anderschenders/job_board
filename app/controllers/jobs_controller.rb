100244b6827f171fc4c623f8a603e306edd0f3a5
Andrea Chen <andreachen.ac@gmail.com>
2017-09-30 11:38:38 -0700

    Adding the create action to make a new job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -10,4 +10,19 @@ class JobsController < ApplicationController
   def new
     @job = Job.new
   end
+
+  def create
+    @job = Job.new(
+    title: params[:job][:title],
+    description: params[:job][:description],
+    company: params[:job][:company]
+    )
+    if @job.save
+      #success
+      redirect_to jobs_path #=>gives you back to rails request cycle to reexcute whole cycle for rails route (goes through entire cycle again)
+    else
+      #failure
+      render :new #=>takes what data controller currently has and sends only that view (:new) and sending it back to user (goes back to view and sends that back with current data)
+    end
+  end
 end
